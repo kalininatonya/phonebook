21f1840f06c54df33e579a1f758a52b9afd97b74
Kalinina Antonina <staraya.kalinina@yandex.ru>
2023-07-06 20:38:34 +0500

    added common constants

---
+++ client/src/constants.ts
@@ -0,0 +1,13 @@
+export const STORAGE_NAME = 'userData';
+export const MAX_NUMBER_OF_CONTACTS_ON_PAGE = 5;
+export const FIRST_PAGE = 1;
+
+//Для лоадера
+export const sizes = {
+    small: 'small',
+    medium: 'middle',
+    large: 'large'
+};
+
+
+
