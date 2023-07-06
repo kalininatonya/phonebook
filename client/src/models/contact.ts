b1a3a5d52f3a71cd25d64ec526d6622d20d659e9
Kalinina Antonina <staraya.kalinina@yandex.ru>
2023-07-06 20:40:44 +0500

    added common models

---
+++ client/src/models/contact.ts
@@ -0,0 +1,8 @@
+export interface Contact {
+    name: string,
+    surname: string,
+    telephone: string,
+    description: string,
+    userId: number,
+    id: number
+}
---
+++ client/src/models/user.ts
@@ -0,0 +1,6 @@
+export interface User {
+    email: string,
+    password: string,
+    login: string,
+    id: null | number
+}
